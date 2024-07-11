3da4575276ff14581e64fbb6c6e21301502c1f3d
Tomasz Łojko <tl@novelpay.pl>
2024-07-11 13:26:31 +0200

    WAL-240980 tags pushed with personal token

--- version.sh
+++ version.sh
@@ -4,24 +4,16 @@ if [ $# -ne 2 ]; then
   exit;
 fi
 
-echo "CI_COMMIT_REF_NAME = $CI_COMMIT_REF_NAME"
-echo "CI_COMMIT_BRANCH = $CI_COMMIT_BRANCH"
-echo "CI_PROJECT_URL = $CI_PROJECT_URL"
-echo "CI_PROJECT_PATH = $CI_PROJECT_PATH"
-echo "GITLAB_USER_NAME = ${GITLAB_USER_NAME}"
-echo "GITLAB_USER_EMAIL = ${GITLAB_USER_EMAIL}"
+COMMIT_MSG="TILL SDK VERSION -$2"
 
-COMMIT_MSG="TILL SDK VERSION -$1"
-
-if [ "$CI_COMMIT_BRANCH" == "dev" ] || [ "$CI_COMMIT_REF_NAME" == "dev" ]; then
-  echo "master branch";
+if [ "$1" == "master" ]; then
+  echo "creating version tag for master branch";
   git config user.email "${GITLAB_USER_EMAIL}"
   git config user.name "${GITLAB_USER_NAME}"
-  git remote set-url origin https://oauth2:$2@${CI_PROJECT_URL:8}.git
-  git tag -fa -m "${COMMIT_MSG}" "$1" && \
-  git push --force origin "$1" && \
-  echo "tag $1 added to repository";
+  git remote set-url origin https://oauth2:"$PROJECT_ACCESS_TOKEN"@"${CI_PROJECT_URL:8}".git
+  git tag -fa -m "${COMMIT_MSG}" "$2" && \
+  git push --force origin "$2" && \
+  echo "tag $2 added to repository";
 else
-  echo "non-master branch"
+  echo "creating version tag skipped due non-master branch: $1"
 fi
-
