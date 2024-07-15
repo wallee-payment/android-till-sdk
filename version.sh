ef796c572f0717fb8a3f2e9e68529c135f7ffa82
Tomasz Łojko <tl@novelpay.pl>
2024-07-15 09:10:53 +0200

    WAL-240980 back to ssh

--- version.sh
+++ version.sh
@@ -6,11 +6,13 @@ fi
 
 COMMIT_MSG="TILL SDK VERSION -$2"
 
-if [ "$1" == "master" ]; then
+if [ "$1" == "dev" ]; then
   echo "creating version tag for master branch";
-  git config user.email "${GITLAB_USER_EMAIL}"
-  git config user.name "${GITLAB_USER_NAME}"
-  git remote set-url origin https://oauth2:"$PROJECT_ACCESS_TOKEN"@"${CI_PROJECT_URL:8}".git
+  #git config user.email "${GITLAB_USER_EMAIL}"
+  #git config user.name "${GITLAB_USER_NAME}"
+  #git remote set-url origin https://oauth2:"$PROJECT_ACCESS_TOKEN"@"${CI_PROJECT_URL:8}".git
+  #ssh git@gitlab.com && \
+  echo "ssh key = $GITLAB_SSH_DEPLOYMENT_KEY" && \
   git tag -fa -m "${COMMIT_MSG}" "$2" && \
   git push --force origin "$2" && \
   echo "tag $2 added to repository";
